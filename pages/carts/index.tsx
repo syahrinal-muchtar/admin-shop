ec4ee2e2c5b2b193f56071b40cb0aa92e344e96a
syahrinal-muchtar <syahrinal.muchtar@bhinneka.com>
2023-08-02 15:58:52 +0700

    Init page carts

---
+++ pages/carts/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SidebarLayout from "@/layouts/SidebarLayout";
+
+function ApplicationsProducts() {
+  return <></>;
+}
+
+ApplicationsProducts.getLayout = (page) => (
+  <SidebarLayout>{page}</SidebarLayout>
+);
+
+export default ApplicationsProducts;
